f229415d910aeb18ab9757d2d5b7729238764942
Belkıs Arslan <97946709+belkisarslan@users.noreply.github.com>
2022-02-23 10:06:38 +0300

    olmadı... sonra bakılacak

--- 12-form-egzersiz/app.js
+++ 12-form-egzersiz/app.js
@@ -1,6 +1,7 @@
 const input = document.querySelector('.input-btn input');
 const listTasks = document.querySelector('.list-tasks ol');
 const message = document.querySelector('.list-tasks');
+const deleteItem = document.querySelector('#deleteItem')
 
 
 function addItem(){
@@ -16,10 +17,18 @@ function addItem(){
     liDOM.classList.add('li')
     listTasks.appendChild(liDOM)
     liDOM.addEventListener('click',function(){
-        liDOM.style.textDecoration = 'line-through'
+            liDOM.style.textDecoration = 'line-through'
     })
-    
+
+    saveLocalList(task)
+
+    deleteItem.addEventListener('click',function(){
+        liDOM.remove()
+        removeLocalList()
+    })
 }
+
+
 
 function showError(error){
     const messageError = document.createElement('p');
@@ -32,3 +41,44 @@ function showError(error){
 
     // console.log(error);
 }
+
+function saveLocalList(task){
+    let localList;
+    if(localStorage.getItem('localList') === null){
+       localList = []
+    }else{
+        localList = JSON.parse(localStorage.getItem('localList'))
+    }
+    localList.push(task)
+    localStorage.setItem('localList',JSON.stringify(localList))
+}
+
+function getList(){
+    let localList;
+    if(localStorage.getItem('localList') === null){
+       localList = []
+    }else{
+        localList = JSON.parse(localStorage.getItem('localList'))
+    }
+    localList.forEach((task) => {
+        let liDOM = document.createElement('li')
+        liDOM.innerHTML = task
+        liDOM.classList.add('li')
+        listTasks.appendChild(liDOM)
+        liDOM.addEventListener('click',function(){
+            liDOM.style.textDecoration = 'line-through'
+        })
+    });
+}
+
+function removeLocalList(){
+    let localList;
+    if(localStorage.getItem('localList') === null){
+       localList = []
+    }else{
+        localList = JSON.parse(localStorage.getItem('localList'))
+    }
+    //window.localStorage.removeItem('localList')
+    localStorage.setItem('localList',JSON.stringify(localList))
+}
+getList()
